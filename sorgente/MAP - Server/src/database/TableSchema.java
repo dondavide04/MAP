e02d337b6efd23d5334da28575ff0572dd0ea02e
Francesco Lomuscio <francilomuscio@gmail.com>
2018-06-26 11:34:38 +0200

    Fix some format defects

--- sorgente/MAP - Server/src/database/TableSchema.java
+++ sorgente/MAP - Server/src/database/TableSchema.java
@@ -99,7 +99,6 @@ public class TableSchema {
 	public TableSchema(DbAccess db, String tableName) throws SQLException {
 		this.db = db;
 		HashMap<String, String> mapSQL_JAVATypes = new HashMap<String, String>();
-		// http://java.sun.com/j2se/1.3/docs/guide/jdbc/getstart/mapping.html
 		mapSQL_JAVATypes.put("CHAR", "string");
 		mapSQL_JAVATypes.put("VARCHAR", "string");
 		mapSQL_JAVATypes.put("LONGVARCHAR", "string");
